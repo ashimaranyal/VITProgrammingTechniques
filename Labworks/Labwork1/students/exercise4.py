922b461b2e94abcfde4599d1505791c808f50357
ashimaranyal <ashima.ranyal@gmail.com>
2024-08-20 15:14:06 +0530

    Update exercise4.py

--- Labworks/Labwork1/students/exercise4.py
+++ Labworks/Labwork1/students/exercise4.py
@@ -2,8 +2,6 @@
 # Exercise 4: searching for an element, with repetition
 # - inputs: your function receives an array and a value;
 # - objective: return the array of the indices of each occurrence of 'value' into the array 'array'.
-
-def exercise4(array, value):
     """
     Returns the array of the indices of each occurrence of 'value' into the array 'array'.
     
@@ -16,4 +14,15 @@ def exercise4(array, value):
         [1,3]
     ```
     """
-    return []
+array=[1,2,1,2,1,2]
+value=1
+def exercise4(array, value):
+    a=[]
+    for index in range(len(array)):
+        if array[index]==value:
+            a.append(index)
+            
+    
+    return a
+
+print(exercise4(array,value))
