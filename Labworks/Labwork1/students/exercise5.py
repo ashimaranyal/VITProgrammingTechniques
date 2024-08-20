a7050ff7e7bf4606ee22ccb5f1c0f43dda251061
ashimaranyal <ashima.ranyal@gmail.com>
2024-08-20 15:24:01 +0530

    Update exercise5.py

--- Labworks/Labwork1/students/exercise5.py
+++ Labworks/Labwork1/students/exercise5.py
@@ -9,6 +9,8 @@
 #   Instead of checking all the values sequentially, you should use the SORT property to find the element, 
 #   if it exists, in O(log n) times for an array of size n.
 
+array=[3,4,5,6,7,8,9,10,1,2,1,4,1,2,4,1]
+value=1
 def exercise5(array, value):
     """
     Returns a tuple containing the INDEX of the first occurrence of the value in the array, 
@@ -17,5 +19,12 @@ def exercise5(array, value):
     NB: This version returns a tuple containing the answer AND the number of tests...
     """
     nb_tests = 0
-        
-    return -1, nb_tests
+    if value not in array:
+        return -1, nb_tests
+    for index in range(len(array)):
+        if array[index]!=value:
+            nb_tests+=1
+        else:
+            return index, nb_tests
+
+print(exercise5(array,value))
